9e0f5a629e0fcac9f4f99302d43db8561d6f454a
«Pavel <kskles@yandex.ru>
2023-06-03 17:11:27 +0700

    Задача54

---
+++ Tesk54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+
+
+
+
+void input_matrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);  //[-10;10]
+}
+
+void print_matrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void TransponMatrix(ref int[,] matrix)
+{
+    
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i + 1; j < matrix.GetLength(1); j++)
+        {
+             for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }  
+        }
+    }
+}
+
+int[,] table = new int[3, 4];
+input_matrix(table);
+print_matrix(table);
+TransponMatrix(ref table);
+Console.WriteLine();
+print_matrix(table);
+
+
+// Console.Clear();
+// Console.Write("Введите размер матрицы: ");
+// // int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// // int[,] matrix = new int[size[0], size[1]];
+// Console.WriteLine("Начальная матрица: ");
+// print_matrix(matrix);
+// TransponMatrix(matrix);
---
+++ Tesk57/Program.cs
@@ -0,0 +1,73 @@
+﻿//Задача 57: Составить частотный словарь элементов 
+//двумерного массива. Частотный словарь содержит 
+//информацию о том, сколько раз встречается элемент входных данных.
+
+bool CheckArray(int[] array, int number, int index_array)
+{
+    for (int i = 0; i < index_array + 1; i++)
+        if (array[i] == number)
+            return false;
+    return true;
+
+}
+
+
+void InputMatrix(int[,] matrix, int[] array, ref int index_array)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);  //[1;10]
+            if (CheckArray(array, matrix[i, j], index_array))
+            {
+                array[index_array] = matrix[i, j];
+                index_array++ ;
+            }
+        }
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+void MatrixCounter(int[] array, int[,] matrix, int size_array)
+{
+    for (int k = 0; k < size_array; k ++)
+    {
+        int count = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] == array[k])
+                    count++;
+            }
+        }
+        Console.WriteLine($"Элемент {array[k]} встречается {count} раз");
+    }
+}
+
+
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int[] array = new int[size[0] * size[1]];
+Console.WriteLine("Начальная матрица: ");
+int size_array = 0;
+InputMatrix(matrix, array, ref size_array);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine(string.Join(", ", array));
+MatrixCounter(array,matrix,size_array);
